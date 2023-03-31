2f00e96281c35c28db24ed9b0ca8646a129d92af
Ernest-Kurish <kurish.ernest@gmail.com>
2023-03-31 23:44:32 +0900

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,6 @@
 import Notiflix from 'notiflix';
 import flatpickr from 'flatpickr';
 
-// Get elements from the DOM
 const datePicker = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('[data-start]');
 const timerDays = document.querySelector('[data-days]');
@@ -9,63 +8,53 @@ const timerHours = document.querySelector('[data-hours]');
 const timerMinutes = document.querySelector('[data-minutes]');
 const timerSeconds = document.querySelector('[data-seconds]');
 
-// Disable start button initially
 startButton.disabled = true;
 
-// Set up flatpickr date picker
 flatpickr(datePicker, {
-dateFormat: 'Y-m-d',
-minDate: 'today',
-onClose: function (selectedDates, dateStr, instance) {
-// Check if selected date is in the future
-const selectedDate = new Date(selectedDates[0]);
-const currentDate = new Date();
-if (selectedDate <= currentDate) {
-// Show error message
-Notiflix.Notify.failure('Please choose a date in the future');
-startButton.disabled = true;
-} else {
-startButton.disabled = false;
-}
-},
+  dateFormat: 'Y-m-d H:i',
+  minDate: 'today',
+  enableTime: true,
+  time_24hr: true,
+  onClose: function (selectedDates, dateStr, instance) {
+    const selectedDate = new Date(selectedDates[0]);
+    const currentDate = new Date();
+    if (selectedDate <= currentDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
 });
 
-// Add event listener to start button
 startButton.addEventListener('click', function () {
-// Get selected date from date picker
-const selectedDate = new Date(datePicker.value);
+  const selectedDate = new Date(datePicker.value);
 
-// Disable start button and date picker
-startButton.disabled = true;
-datePicker.disabled = true;
+  startButton.disabled = true;
+  datePicker.disabled = true;
 
-// Set up countdown timer
-const countdownInterval = setInterval(function () {
-// Calculate time remaining
-const timeRemaining = selectedDate.getTime() - new Date().getTime();
-if (timeRemaining <= 0) {
-// Stop countdown when time is up
-clearInterval(countdownInterval);
-// Show success message
-Notiflix.Notify.success('Time is up!');
-// Reset start button and date picker
-startButton.disabled = false;
-datePicker.disabled = false;
-return;
-}
-const timeRemainingObj = convertMs(timeRemaining);
-timerDays.textContent = addLeadingZero(timeRemainingObj.days);
-timerHours.textContent = addLeadingZero(timeRemainingObj.hours);
-timerMinutes.textContent = addLeadingZero(timeRemainingObj.minutes);
-timerSeconds.textContent = addLeadingZero(timeRemainingObj.seconds);
-}, 1000);
+  const countdownInterval = setInterval(function () {
+    const timeRemaining = selectedDate.getTime() - new Date().getTime();
+    if (timeRemaining <= 0) {
+      clearInterval(countdownInterval);
+      Notiflix.Notify.success('Time is up!');
+      startButton.disabled = false;
+      datePicker.disabled = false;
+      return;
+    }
+    const timeRemainingObj = convertMs(timeRemaining);
+    timerDays.textContent = addLeadingZero(timeRemainingObj.days);
+    timerHours.textContent = addLeadingZero(timeRemainingObj.hours);
+    timerMinutes.textContent = addLeadingZero(timeRemainingObj.minutes);
+    timerSeconds.textContent = addLeadingZero(timeRemainingObj.seconds);
+  }, 1000);
 });
 
 function convertMs(ms) {
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
   // Remaining days
   const days = Math.floor(ms / day);
